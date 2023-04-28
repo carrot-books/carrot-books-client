79efa5234ed1e75ff8dafe5e5ba6e9a5cf3f0417
MadHeo <tnlfl288@gmail.com>
2023-04-28 23:47:11 +0900

    navigationBar 수정 & create loading page

--- components/NavigationBar/index.vue
+++ components/NavigationBar/index.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="w-2/3 mx-auto py-8 flex gap-8">
-    <NuxtLink to="/">
-      <button class="text-lg">홈</button>
-    </NuxtLink>
-    <NuxtLink to="/count">
-      <button class="text-lg">카운트</button>
-    </NuxtLink>
-    <NuxtLink to="/login">
-      <button class="text-lg">로그인</button>
+    <NuxtLink
+      class="cursor-pointer"
+      v-for="(menu, i) in menus"
+      :key="i"
+      :to="menu.to"
+    >
+      <p class="text-lg font-bold">{{ menu.name }}</p>
     </NuxtLink>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      menus: [
+        { name: "홈", to: "/" },
+        { name: "로그인", to: "login" },
+        { name: "카운트", to: "count" },
+        { name: "로딩테스트", to: "loading" },
+      ],
+    };
+  },
+};
+</script>
