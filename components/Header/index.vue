60d2749ce4f8e2eeea6c803002b3e91470ae5efd
hannapa1114 <flexaveler@gmail.com>
2023-05-02 10:37:34 +0900

    add: logo

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="border-b-2">
-    <div class="w-2/3 mx-auto py-8">
+    <div class="w-2/3 mx-auto py-8 flex items-center gap-4">
+      <img
+        class="h-12 w-12 rounded-full bg-slate-300"
+        src="~/assets/img/logo.png"
+      />
       <h1 class="text-5xl">당근북스</h1>
     </div>
   </div>
