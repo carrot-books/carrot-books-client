16e7ecc7d001ec565caeaf4b48f6e3b749f9a494
hannapa1114 <flexaveler@gmail.com>
2023-05-09 11:13:00 +0900

    add: vitest 컴포넌트 테스트 환경설정 및 테스트코드 예시 작성

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -3,9 +3,9 @@
     <div class="w-2/3 mx-auto py-8 flex items-center gap-4">
       <img
         class="h-12 w-12 rounded-full bg-slate-300"
-        src="~/assets/img/logo.png"
+        src="../assets/img/logo.png"
       />
-      <h1 class="text-5xl">당근북스</h1>
+      <h1 id="title" class="text-5xl">당근북스</h1>
     </div>
   </div>
 </template>
