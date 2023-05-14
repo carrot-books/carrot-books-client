a2e591afddbdf97afed3a13a81f27bdd3c228ed9
MadHeo <tnlfl288@gmail.com>
2023-05-14 19:37:06 +0900

    feat: 메인 화면 bookList 추가

---
+++ assets/data/bookData.ts
@@ -0,0 +1,77 @@
+export default [
+  {
+    user: "김지욱",
+    title: "스즈메의 문단속",
+    img: "/_nuxt/assets/img/img_sezeme.png",
+  },
+  {
+    user: "대니",
+    title: "칵테일, 러브, 좀비",
+    img: "/_nuxt/assets/img/img_cocktail.png",
+  },
+  {
+    user: "dany",
+    title: "세이노의 가르침",
+    img: "/_nuxt/assets/img/img_seino.png",
+  },
+  {
+    user: "김지욱",
+    title: "스즈메의 문단속",
+    img: "/_nuxt/assets/img/img_sezeme.png",
+  },
+  {
+    user: "대니",
+    title: "칵테일, 러브, 좀비",
+    img: "/_nuxt/assets/img/img_cocktail.png",
+  },
+  {
+    user: "dany",
+    title: "세이노의 가르침",
+    img: "/_nuxt/assets/img/img_seino.png",
+  },
+  {
+    user: "김지욱",
+    title: "스즈메의 문단속",
+    img: "/_nuxt/assets/img/img_sezeme.png",
+  },
+  {
+    user: "대니",
+    title: "칵테일, 러브, 좀비",
+    img: "/_nuxt/assets/img/img_cocktail.png",
+  },
+  {
+    user: "dany",
+    title: "세이노의 가르침",
+    img: "/_nuxt/assets/img/img_seino.png",
+  },
+  {
+    user: "김지욱",
+    title: "스즈메의 문단속",
+    img: "/_nuxt/assets/img/img_sezeme.png",
+  },
+  {
+    user: "대니",
+    title: "칵테일, 러브, 좀비",
+    img: "/_nuxt/assets/img/img_cocktail.png",
+  },
+  {
+    user: "dany",
+    title: "세이노의 가르침",
+    img: "/_nuxt/assets/img/img_seino.png",
+  },
+  {
+    user: "김지욱",
+    title: "스즈메의 문단속",
+    img: "/_nuxt/assets/img/img_sezeme.png",
+  },
+  {
+    user: "대니",
+    title: "칵테일, 러브, 좀비",
+    img: "/_nuxt/assets/img/img_cocktail.png",
+  },
+  {
+    user: "dany",
+    title: "세이노의 가르침",
+    img: "/_nuxt/assets/img/img_seino.png",
+  },
+];
