fa30bed8d27138908c864f866c594bfb5750f02b
MadHeo <tnlfl288@gmail.com>
2023-08-15 22:55:55 +0900

    feat: 내 책 리스트 UI 구현

---
+++ components/MyBookList/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="border w-full py-10">
+    <div class="flex-col px-36 mb-8">
+      <img src="../../assets/img/img_profile.png" alt="" />
+      <div class="flex mt-4">
+        <p class="font-bold">이미리</p>
+        <p>님의 책 리스트</p>
+      </div>
+    </div>
+    <div class="flex flex-row w-full border-b px-36 mb-10">
+      <div class="flex border-b-4 mr-6">
+        등록한 책
+        <p>(0)</p>
+      </div>
+      <div class="flex">
+        당근한 책
+        <p>(0)</p>
+      </div>
+    </div>
+    <div class="px-36">
+      <div
+        class="w-56 h-80 border-dashed border rounded-md flex justify-center items-center"
+      >
+        <div class="">현재 등록한 책이 없습니다</div>
+      </div>
+    </div>
+  </div>
+</template>
