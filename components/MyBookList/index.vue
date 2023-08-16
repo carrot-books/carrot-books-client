64d38a124692115e2a132554bfdfbb9b07e5794b
MadHeo <tnlfl288@gmail.com>
2023-08-16 23:47:06 +0900

    fix: 내 책 리스트 수정

--- components/MyBookList/index.vue
+++ components/MyBookList/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="border w-full py-10">
+  <div class="w-full py-10">
     <div class="flex-col px-36 mb-8">
       <img src="../../assets/img/img_profile.png" alt="" />
       <div class="flex mt-4">
@@ -8,21 +8,42 @@
       </div>
     </div>
     <div class="flex flex-row w-full border-b px-36 mb-10">
-      <div class="flex border-b-4 mr-6">
+      <button class="flex mr-6 cursor-pointer border-b-4">
         등록한 책
-        <p>(0)</p>
-      </div>
-      <div class="flex">
+        <p class="ml-1">(0)</p>
+      </button>
+      <button class="flex cursor-pointer focus:border-b-4">
         당근한 책
-        <p>(0)</p>
-      </div>
+        <p class="ml-1">(0)</p>
+      </button>
     </div>
     <div class="px-36">
       <div
-        class="w-56 h-80 border-dashed border rounded-md flex justify-center items-center"
+        class="w-56 h-80 border-dashed border rounded-md flex flex-col justify-center items-center cursor-pointer"
+        @click="toggleRegisterModal"
       >
         <div class="">현재 등록한 책이 없습니다</div>
+        <div class="">책 등록하기 ></div>
       </div>
     </div>
   </div>
+  <BookRegister v-if="isRegisterModalOpen == true" />
 </template>
+
+<script lang="ts">
+import { useHeaderStore } from "../../stores/header";
+import { computed } from "vue";
+
+export default {
+  setup() {
+    const headerStore = useHeaderStore();
+    const toggleRegisterModal = computed(() => headerStore.toggleRegisterModal);
+    const isRegisterModalOpen = computed(() => headerStore.isRegisterModalOpen);
+
+    return {
+      toggleRegisterModal,
+      isRegisterModalOpen,
+    };
+  },
+};
+</script>
