b68004d5304885511292e929088dd0989b41bd90
MadHeo <tnlfl288@gmail.com>
2023-08-23 23:55:58 +0900

    feat: 책 리스트 페이지 api 연동

--- components/BookList/index.vue
+++ components/BookList/index.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="w-full h-full p-20 flex flex-row gap-5 flex-wrap">
     <div
-      class="w-64 h-96 flex flex-col justify-end rounded hover:bg-gray150 cursor-pointer"
-      v-for="books in Bookdata"
+      class="w-64 flex flex-col justify-end rounded hover:bg-gray150 cursor-pointer"
+      v-for="books in bookListData"
       :key="books.id"
       @click="onClickBook(books.id)"
     >
       <img
-        class="object-cover w-full max-h-80 mb-4 border rounded bg-gray250"
-        :src="books.img"
+        class="object-cover w-full max-h-96 mb-4 border border-#c8c8c8 rounded bg-gray150"
+        :src="books.cover"
         alt=""
       />
-
       <div class="flex flex-row">
         <div class="mr-2">
           <img
@@ -20,7 +19,7 @@
             alt=""
           />
         </div>
-        <div>{{ books.user }}</div>
+        <div>{{ books.registered.owner.name }}</div>
       </div>
     </div>
   </div>
@@ -28,15 +27,30 @@
 
 <script lang="ts">
 import Bookdata from "../../src/data/bookData";
+import axios from "axios";
+import { IBookListData } from "../../src/types/types";
 
 export default {
   data() {
     return {
       Bookdata,
+      bookListData: [] as IBookListData[],
     };
   },
+  created() {
+    axios
+      .get(
+        "https://stoplight.io/mocks/carotin-party/carrot-books/222910724/registered-books"
+      )
+      .then((res) => {
+        this.bookListData = res.data.books;
+      })
+      .catch((error) => {
+        console.log(error, "책 데이터를 불러오지 못했습니다.");
+      });
+  },
   methods: {
-    onClickBook(bookId: string) {
+    onClickBook(bookId: number) {
       this.$router.push(`/detail/${bookId}`);
     },
   },
